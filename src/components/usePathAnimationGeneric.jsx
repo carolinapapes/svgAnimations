b8437877dc72711ca1c0ba9ebe167cb5ad3f98d6
carolinapapes <papes.caro@gmail.com>
2023-03-21 21:20:50 -0300

    reorder imports

--- src/components/usePathAnimationGeneric.jsx
+++ src/components/usePathAnimationGeneric.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './PathAnimation.scss';
 
 function usePathAnimationGeneric(path, cssVarible) {
